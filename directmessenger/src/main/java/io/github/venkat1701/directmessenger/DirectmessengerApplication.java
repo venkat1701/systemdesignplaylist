a742215422b1d82d4bb26e37dd3e2fc8232e3378
venkat1701 <thejeastdev@gmail.com>
2025-03-22 23:33:05 +0530

    initializing direct messenger for 1:1 chats

---
+++ directmessenger/src/main/java/io/github/venkat1701/directmessenger/DirectmessengerApplication.java
@@ -0,0 +1,13 @@
+package io.github.venkat1701.directmessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectmessengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DirectmessengerApplication.class, args);
+    }
+
+}
