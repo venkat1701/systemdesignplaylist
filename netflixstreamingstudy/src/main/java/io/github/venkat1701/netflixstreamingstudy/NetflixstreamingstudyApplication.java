05e39c5e12c9c5d6babf11222d7cce58d7952eb1
venkat1701 <thejeastdev@gmail.com>
2025-06-05 23:30:49 +0530

    initialized netflix case study about live streaming via HTTP Range Streaming

---
+++ netflixstreamingstudy/src/main/java/io/github/venkat1701/netflixstreamingstudy/NetflixstreamingstudyApplication.java
@@ -0,0 +1,13 @@
+package io.github.venkat1701.netflixstreamingstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixstreamingstudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetflixstreamingstudyApplication.class, args);
+    }
+
+}
